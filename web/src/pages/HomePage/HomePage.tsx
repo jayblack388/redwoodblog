7b1eb4d18697b73dd4f3ced63ce8aab8584a8644
John Blackwell <jblackwell072588@gmail.com>
2024-04-17 23:20:20 -0400

    fix(chapter-01): Made some pages and a layout

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+      Home
+    </>
+  )
+}
+
+export default HomePage
